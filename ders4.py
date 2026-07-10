48fa46bba54d16050446cb1ea4ddb1f80a0c9c41
Shamil03260 <ferideshamil1286@gmail.com>
2026-07-10 20:48:53 +0400

    2gtf

--- ders4.py
+++ ders4.py
@@ -2,7 +2,7 @@ my_list = ["apple", "cherry", "orange", "banana"]
 
 last_value = my_list[-1]
 
-if last_value.startswith("b") == True:
+if last_value.startswith("b"):
     print("True")
 else:
     print("False")
