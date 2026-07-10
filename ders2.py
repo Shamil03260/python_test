e833a4f0480cf8823175894b509ea465e9694732
Shamil03260 <ferideshamil1286@gmail.com>
2026-07-10 17:02:51 +0400

    442g

---
+++ ders2.py
@@ -0,0 +1,77 @@
+# number = 5
+
+# if number > 10:
+#     print("Şərt ödəndi")
+    
+# elif number > 5:
+#     print("ədəd 5-dən böyükdür")
+    
+# else:
+#     print("Heç bir şərt ödənmir")
+    
+    
+# age = 20
+
+# if age > 18:
+#     print("Yetkin")
+# else:
+#     print("Yetkin deyil")
+
+
+# name = "Shamil"
+
+# if "n" in name:
+#     print("Şərt ödənir")
+# else:
+#     print("Şərt ödənmir")
+    
+    
+# city = "Baku"
+
+# if "z" not in city:
+#     print("Şərt ödənir")
+# else:
+#     print("Şərt ödənmir")
+
+
+# city = "London"
+
+# if "r" in city or "b" in city or "a" in city:
+#     print("Şərt ödənir")
+    
+# else:
+#     print("Şərt ödənmir")
+    
+    
+# name = "Ayan"
+
+# if "z" in name and "a" in name:
+#     print("Şərt ödənir")
+    
+# else:
+#     print("Şərt ödənmir")
+
+
+# number = int(input("Enter the number: "))
+
+# if number > 0:
+#     print("positive")
+
+# elif number < 0:
+#     print("Negative")
+
+# else:
+#     print("Zero")
+
+
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+
+# print("Maksimum ədəd: ", max(num1, num2))
+# print("Minimum ədəd: ", min(num1, num2))
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print(pow(num1, num2)) 
