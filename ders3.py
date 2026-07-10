cc35aedad02bdd7e07b15b56795557db02c378d4
Shamil03260 <ferideshamil1286@gmail.com>
2026-07-10 20:08:44 +0400

    2vr

---
+++ ders3.py
@@ -0,0 +1,20 @@
+names = ["Nəsib", "Şamil", "Günel", "Ayan", "Nurlan"]
+
+
+print("1. Ad əlavə etmək")
+print("2. Ad silmək")
+choice = int(input("Seçim edin: "))
+
+if choice == 1:
+    add_name = input("Ad daxil edin: ")
+    names.append(add_name)
+    print(names)
+elif choice == 2:
+    name = input("Ad daxil edin: ")
+    if name not in names:
+        print("Ad tapılmadı")
+    else:
+        names.remove(name)
+        print(names)
+else:
+    print("Error")
