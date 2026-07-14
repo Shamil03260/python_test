0b74d571e887b9948fac2bae82c33b7527ba6e02
Shamil03260 <ferideshamil1286@gmail.com>
2026-07-14 13:33:56 +0400

    2f2gr

--- ders1.py
+++ ders1.py
@@ -1,1 +1,108 @@
-print("Hello, World!")
+# print("Hello World") 
+
+# = menimsetmey
+#  == beraberlik
+# != beraber deyil
+#  , sadece yan yana yazdirir
+# + birlesdirir ve ya ustune gelir
+
+
+
+
+
+# x = 5
+
+
+# x = 10
+
+# print(x)
+
+
+
+# str()
+# x = "Python"
+
+# int()
+# number = 5
+
+#float()
+# float_number = 2.5
+
+# bool
+# bool_number = True
+
+# print(number == float_number)
+
+# print(id(x))
+
+# print(type(bool_number))
+# print(type(number))
+# print(type(x))
+# print(type(float_number))
+
+
+# prog_lang = "java"
+
+# print(prog_lang[0])
+
+# print(prog_lang.count("v",0))
+
+
+#print(prog_lang.capitalize())
+
+
+# news = "you must enable javascript 33 il əvvəl Kəlbəcərdə itkin düşmüş Mədinə Babayeva dəfn olundu "
+
+# print(news.replace("you must enable javascript", ""))
+
+
+# city = "Baku"
+
+
+# print(city.endswith("u"))
+
+# print(city.upper())
+# print(city.lower())
+
+# name = input("Adinizi daxil edin : ")
+
+# print("Hello", name)
+
+
+
+# number = int(input("Eded daxil edin : "))
+
+# print("Hello" + number )
+
+# number1 = 5
+# number2 = 10
+# print(number1 + number2)
+
+# city = "Baku,London"
+
+
+# print(city.split(','))
+
+# city = "   Baku       "
+
+# print(city)
+
+# print(city.strip())
+
+# name = "Ayan"
+
+# print(len(name))
+
+# name = "Shamil"
+
+# city = "Baki"
+
+# # f string
+# print(f"{name} {city} seherinde yasayir")
+
+# print(5/2)
+# print(5//2)
+# print(5 % 2)
+# print(5 != 2)
+
+
--- ders2.py
+++ ders2.py
@@ -1,79 +1,244 @@
-# number = 5
+# # number = 5
 
-# if number > 10:
-#     print("Şərt ödəndi")
+# # if number > 10:
+# #     print("Şərt ödəndi")
     
-# elif number > 5:
-#     print("ədəd 5-dən böyükdür")
+# # elif number > 5:
+# #     print("ədəd 5-dən böyükdür")
     
-# else:
-#     print("Heç bir şərt ödənmir")
-    
-    
-# age = 20
-
-# if age > 18:
-#     print("Yetkin")
-# else:
-#     print("Yetkin deyil")
-
-
-# name = "Shamil"
-
-# if "n" in name:
-#     print("Şərt ödənir")
-# else:
-#     print("Şərt ödənmir")
+# # else:
+# #     print("Heç bir şərt ödənmir")
     
     
-# city = "Baku"
+# # age = 20
 
-# if "z" not in city:
-#     print("Şərt ödənir")
-# else:
-#     print("Şərt ödənmir")
+# # if age > 18:
+# #     print("Yetkin")
+# # else:
+# #     print("Yetkin deyil")
 
 
-# city = "London"
+# # name = "Shamil"
 
-# if "r" in city or "b" in city or "a" in city:
-#     print("Şərt ödənir")
+# # if "n" in name:
+# #     print("Şərt ödənir")
+# # else:
+# #     print("Şərt ödənmir")
     
-# else:
-#     print("Şərt ödənmir")
+    
+# # city = "Baku"
+
+# # if "z" not in city:
+# #     print("Şərt ödənir")
+# # else:
+# #     print("Şərt ödənmir")
+
+
+# # city = "London"
+
+# # if "r" in city or "b" in city or "a" in city:
+# #     print("Şərt ödənir")
+    
+# # else:
+# #     print("Şərt ödənmir")
     
     
-# name = "Ayan"
+# # name = "Ayan"
 
-# if "z" in name and "a" in name:
-#     print("Şərt ödənir")
+# # if "z" in name and "a" in name:
+# #     print("Şərt ödənir")
     
-# else:
-#     print("Şərt ödənmir")
+# # else:
+# #     print("Şərt ödənmir")
 
 
-# number = int(input("Enter the number: "))
+# # number = int(input("Enter the number: "))
 
-# if number > 0:
-#     print("positive")
+# # if number > 0:
+# #     print("positive")
 
-# elif number < 0:
-#     print("Negative")
+# # elif number < 0:
+# #     print("Negative")
 
-# else:
-#     print("Zero")
-
-
-# num1 = int(input("Enter the first number: "))
-# num2 = int(input("Enter the second number: "))
-
-# print("Maksimum ədəd: ", max(num1, num2))
-# print("Minimum ədəd: ", min(num1, num2))
+# # else:
+# #     print("Zero")
 
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+# # num1 = int(input("Enter the first number: "))
+# # num2 = int(input("Enter the second number: "))
 
-print(pow(num1, num2)) 
+# # print("Maksimum ədəd: ", max(num1, num2))
+# # print("Minimum ədəd: ", min(num1, num2))
 
 
+# # num1 = int(input("Enter the first number: "))
+# # num2 = int(input("Enter the second number: "))
+
+# # print(pow(num1, num2)) 
+
+
+
+# if elif else taskları
+
+# task 1
+
+age = int(input("Yaşınızı dsaxil edin: "))
+
+if age > 18:
+    print("Giriş icazəlidir")
+else:
+    print("Giriş qadağandır")
+
+
+# task 2
+
+num1 = 34
+num2 = 65
+
+
+max_numbers = max(num1, num2)
+
+print(f"Maksimum ədəd: {max_numbers}")
+
+
+
+# task 3
+
+score = 30
+
+if score >= 91:
+    print("A")
+elif score >= 71:
+    print("B")
+elif score >= 51:
+    print("C")
+elif score >= 31:
+    print("C")
+else:
+    print("F")
+    
+
+# task 4
+
+number = 5
+
+if number > 0:
+    print("Ədəd müsbətdir")
+elif number < 0:
+    print("Ədəd mənfidir")
+else:
+    print("Ədəd sıfırdır")
+    
+
+# task 5
+
+month_digit = 7
+
+if month_digit == 12 or month_digit == 1 or month_digit == 2:
+    print("Qış")
+elif month_digit == 3 or month_digit == 4 or month_digit == 5:
+    print("Yaz")
+elif month_digit == 6 or month_digit == 7 or month_digit == 8:
+    print("Yay")
+elif month_digit == 9 or month_digit == 10 or month_digit == 11:
+    print("Payız")
+    
+
+# task 6
+
+username = input("İstifadəçi adı: ").capitalize
+password = input("Şifrə: ")
+
+correct_username = "Şamil"
+correct_password = "1212"
+
+if username == correct_username and password == correct_password:
+    print("Giriş uğurludur")
+else:
+    print("İstifadəçi adı və ya şifrə yanlışdır")
+    
+
+# task 7
+
+number = 86
+
+if number % 2 == 0:
+    print("Bu ədəd cütdür")
+else:
+    print("Bu ədəd təkdir")
+
+
+# task 8
+
+num1 = 54
+num2 = 76
+num3 = 25
+
+num_max = max(num1, num2, num3)
+
+print(f"Maksimum ədəd: {num_max}")
+
+
+# task 9
+
+login = input("Login: ")
+password = input("Parol: ")
+
+correct_login = "nasib"
+correct_password = "12345"
+
+if login == "" and password == "":
+    print("Login ve parol daxil etmediniz")
+
+elif login == "":
+    print("Login daxil etmediniz")
+
+elif password == "":
+    print("Parol daxil etmediniz")
+
+elif login != correct_login and password != correct_password:
+    print("Login ve parol yanlishdir")
+
+elif login != correct_login:
+    print("Login yanlishdir")
+
+elif password != correct_password:
+    print("Parol yanlishdir")
+
+else:
+    print("Xosh geldiniz Nasib")
+    
+
+# task 10
+
+name = input("Ad daxil edin: ").capitalize
+
+if name == "Aslan":
+    print("Aslan emi ogludur") 
+elif name == "Imran":
+    print("Imran dayi ogludur")
+elif name == "Afaq":
+    print("Afaq bibi qizidir")
+elif name == "Uzeyir":
+    print("Uzeyir xala ogludur")
+elif name == "Shahin":
+    print("Shahin yaxin dostdur")
+else:
+    print(f"{name} kimdir? Mən onu tanımıram")
+    
+
+# task 11
+
+height = float(input("Boyunuzu daxil edin (m): "))
+weight = float(input("Kütlənizi daxil edin (kg): "))
+
+bki = weight / (height * height)
+
+if bki < 18.5:
+    print("Zəif")
+elif bki < 25:
+    print("Normal")
+elif bki < 30:
+    print("Kilolu")
+else:
+    print("Obez")
