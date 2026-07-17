f85d4a5a83d01b99c5969fb28513a7a5d88527ad
Shamil03260 <ferideshamil1286@gmail.com>
2026-07-17 20:50:33 +0400

    32gv

--- ders7.py
+++ ders7.py
@@ -1,12 +1,22 @@
-i = 0
+# i = 0
 
-number = int(input("Number: "))
-if number > 10 or number == 0:
-    print("Error")
+# number = int(input("Number: "))
+# if number > 10 or number == 0:
+#     print("Error")
     
-else:
-    while i < 10:
-        i += 1
-        answer = number * i 
-        print(number, "x", i, "=", answer)
-        
+# else:
+#     while i < 10:
+#         i += 1
+#         answer = number * i 
+#         print(number, "x", i, "=", answer)
+
+# row = 1
+
+# while row <= 10:
+#     col = 1
+#     while col <= 10:
+#         result = row * col
+#         print(f"{row} x {col} = {result}", end="\t")
+#         col += 1
+#     print()  
+#     row += 1
---
+++ ders8.py
@@ -0,0 +1,7 @@
+text = input("Mətn daxil et: ")
+c = int(input("Sayını daxil et: "))
+i = 0
+
+while i != c:
+    i += 1
+    print(f"{i}. {text}")
