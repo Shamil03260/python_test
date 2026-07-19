db971e3f88771a5ab34791ab102af23220b921e5
Shamil03260 <ferideshamil1286@gmail.com>
2026-07-20 00:29:34 +0400

    2vr2

--- ders8.py
+++ ders8.py
@@ -1,7 +1,44 @@
-text = input("Mətn daxil et: ")
-c = int(input("Sayını daxil et: "))
-i = 0
+# list comprehension
 
-while i != c:
-    i += 1
-    print(f"{i}. {text}")
+
+
+# my_list = [1, 2, 3, 4, 5]
+
+# for i in my_list:
+#     if i % 2 == 0:
+        
+#         print("success")
+
+  
+# result=[]
+# for x in range(1,6):
+#     result.append(x*x)
+
+
+# kvadratlar = [x*x for x in range(1,6)]
+
+# my_list = [i for i in range(1,10) if i % 2 == 0 ]
+
+# print(my_list)
+
+
+# result={}
+
+# for x in range(1,6):
+#     result[x]=x*x
+    
+# print(result)
+
+
+# kvadratlar = {x: x*x for x in range(1,6)}
+# print(kvadratlar)
+
+
+
+# text = input("Mətn daxil et: ")
+# c = int(input("Sayını daxil et: "))
+# i = 0
+
+# while i != c:
+#     i += 1
+#     print(f"{i}. {text}")
